ef0073fc23289248c73111ed326aae1066457449
Lukas Baur <lukas.baur@ipa.fraunhofer.de>
2026-07-03 14:49:29 +1000

    ✳ Tiny plot improvements

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# 0.1.1
+✨ tiny plot improvements: single forecast ready (non-quantiles)
+
 # <= 0.1.0
 *initial verison, focus on simple access functions*
 
--- src/epex_forecast/simple_plot.py
+++ src/epex_forecast/simple_plot.py
@@ -5,22 +5,46 @@ import matplotlib.pyplot as plt
 
 def plot_forecast(forecast_df: pd.DataFrame, meta: dict = None):
     forecat_columns = [col for col in forecast_df.columns if col != "ground truth"]
+
     plt.figure(figsize=(12, 5))
 
     if len(forecat_columns) > 1:
+        # plot quantiles as shaded area
         assert len(forecat_columns) % 2 == 1 # only odd number of quantiles (paarwise + one main forecast)
 
+        # extract main forecast
         main_id = len(forecat_columns) // 2 # center quantile = main forecast
         main_col = forecat_columns[main_id]
 
+        # extract quantile pairs and plot them
         color_alphas = np.linspace(0, 0.15, main_id+1, endpoint=True)[1:]
         for i in range(main_id):
             lower_col = forecat_columns[i]
             upper_col = forecat_columns[-i - 1]
-            plt.fill_between(forecast_df.index, forecast_df[lower_col], forecast_df[upper_col], color="blue", alpha=color_alphas[i])
-        plt.plot(forecast_df.index, forecast_df[main_col], label="forecast", color="blue", linewidth=3, alpha=0.7)
+            plt.fill_between(forecast_df.index,
+                             forecast_df[lower_col],
+                             forecast_df[upper_col],
+                             color="blue",
+                             alpha=color_alphas[i])
+    else:
+        main_col = forecat_columns[0]
 
-    plt.plot(forecast_df.index, forecast_df["ground truth"], label="ground truth", color="red", linewidth=3, alpha=0.8)
+    # plot main forecast
+    plt.plot(forecast_df.index,
+             forecast_df[main_col],
+             label="forecast",
+             color="blue",
+             linewidth=3,
+             alpha=0.8)
+
+    # plot ground truth
+    plt.plot(forecast_df.index,
+             forecast_df["ground truth"],
+             label="ground truth",
+             color="red",
+             linewidth=3,
+             alpha=0.7)
+
     plt.xlim(forecast_df.index[0], forecast_df.index[-1])
     plt.grid()
     plt.xlabel("Time")
